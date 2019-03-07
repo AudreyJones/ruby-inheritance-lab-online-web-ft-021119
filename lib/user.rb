bfd41ba8b30666560a30686b95a9ba8550bbe590
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-07 02:50:47 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -4,11 +4,11 @@ class User
   #   @first_name = first_name
   #   @last_name = last_name
   # end
-  # 
+  #
   # def first_name
   #   @first_name
   # end
-  # 
+  #
   # def last_name
   #   @last_name
   # end
